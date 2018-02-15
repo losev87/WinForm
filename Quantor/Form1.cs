ff334130f9f7aba4bcc2f964b3bece1073d28a62
losev87 <losev.87@mail.ru>
2018-02-15 08:11:01 +0300

    quants

---
+++ Quantor/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quantor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog myDialog = new OpenFileDialog();
+            myDialog.Filter = "CSV|*.csv";
+            myDialog.CheckFileExists = true;
+            myDialog.Multiselect = false;
+            if (myDialog.ShowDialog() == DialogResult.OK)
+            {
+                Create(myDialog.FileName);
+            }
+        }
+
+        class DTO
+        {
+            public DateTime Date;
+            public double Value;
+        }
+
+        private void Create(string fileName)
+        {
+            List<DTO> result;
+            using (System.IO.StreamReader file = new System.IO.StreamReader(fileName, Encoding.UTF8))
+            {
+                string line;
+                //file.ReadLine();
+                var ind = 0;
+                var quant = double.Parse(textBox1.Text.Replace(".", ","), new CultureInfo("ru-RU"));
+                var firstLine = file.ReadLine();
+                result = new[] {new DTO {Date = new DateTime(2000, 1, 1),Value= double.Parse(firstLine.Split(',')[5].Replace(".", ","), new CultureInfo("ru-RU")) } }.ToList();
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.Length <= 0) continue;
+                    
+                    
+                        var cols = line.Split(',');
+                        var val = double.Parse(cols[5].Replace(".", ","), new CultureInfo("ru-RU"));
+
+                    var dif = Math.Abs(val - result.Last().Value);
+                    if (dif > quant)
+                    {
+                        var sign = val < result.Last().Value ? -1 : 1;
+                        var steps = (int)(dif / quant);
+                        for (int i = 1; i <= steps; i++)
+                        {
+                            var newQuantVal = result.Last().Value + quant * sign;
+                            result.Add(new DTO()
+                            {
+                                Date = result.Last().Date.AddDays(1),
+                                Value = newQuantVal
+                            });
+                        }
+                    }
+                }
+            }
+
+            Directory.CreateDirectory(Path.Combine(
+                Path.GetDirectoryName(fileName),
+                "quants"));
+            File.WriteAllLines(
+                Path.Combine(
+                    Path.GetDirectoryName(fileName),
+                    "quants",
+                    Path.GetFileName(fileName)
+                ),
+                result.Select(r => $"{r.Date:yyyy.MM.dd HH:mm};{r.Value:0.00000}")
+            );
+        }
+    }
+}
---
+++ Quantor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Quantor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Quantor")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d341f763-f5d3-4d51-b7b2-03b3eb74c6e9")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
